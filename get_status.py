87d7b0cb0d1c64e1fa6a7a5149af6585f7125807
Tyler Neely <60554951+softwaresuave@users.noreply.github.com>
2023-08-27 20:45:28 -0500

    Add files via upload

---
+++ get_status.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+#using cisco sandbox device
+router = {"ip": "ios-xe-mgmt.cisco.com", "port": "9443",
+          "user": "root", "password": "D_Vay!_10&"}
+
+# set REST API headers
+headers = {"Accept": "application/yang-data+json",
+           "Content-Type": "application/yang-data+json"}
+
+#selecting interfaces datastore
+url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces"
+# print(url)
+
+response = requests.get(url, headers=headers, auth=(
+    router['user'], router['password']), verify=False)
+
+api_data = response.json()
+print("/" * 55)
+print(api_data["Cisco-IOS-XE-interfaces-oper:interface"]["description"])
+print("/" * 55)
+if api_data["Cisco-IOS-XE-interfaces-oper:interface"]["admin-status"] == 'if-state-up':
+    print('Interface is up')
+else:
+    print('Interface is down')
